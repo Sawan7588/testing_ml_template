0caf06e71920f012524d4b9169de420b7bb85605
Sawan7588 <105876636+Sawan7588@users.noreply.github.com>
2023-05-02 17:36:08 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from Visa.logger import logging
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    logging.info("We are testing our logging file")
+    return "Hello World"
+
+if __name__=="__main__":
+    app.run(debug=True)
